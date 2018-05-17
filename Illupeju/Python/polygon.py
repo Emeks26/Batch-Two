a62ee27eed7810273137497d59c2290fa65d56e4
kadiri Sunday Eshiomonu <32268527+kadirisunday@users.noreply.github.com>
2018-05-17 15:10:28 +0100

    Add files via upload

---
+++ Illupeju/Python/polygon.py
@@ -0,0 +1,36 @@
+# Ajanaku Fatai
+# ajanakufatai@outlook.com
+# 09095634347
+
+import turtle
+A=turtle
+A.reset()
+A.color("blue")
+A.begin_fill()
+A.forward(100)
+A.left(90)
+A.forward(100)
+A.left(90)
+A.forward(100)
+A.left(90)
+A.forward(100)
+A.left(90)
+A.forward(100)
+A.color("red")
+A.begin_fill()
+A.left(60)
+A.forward(100)
+A.left(60)
+A.forward(100)
+A.left(60)
+A.forward(100)
+A.left(60)
+A.forward(100)
+A.left(60)
+A.forward(100)
+A.left(60)
+A.forward(100)
+A.left(60)
+A.forward(100)
+
+
