29d784a777a829b7e298678891c764abb1e37bf5
kadiri Sunday Eshiomonu <32268527+kadirisunday@users.noreply.github.com>
2018-05-17 15:14:19 +0100

    Add files via upload

---
+++ Illupeju/Python/Calender_creation_each_year.py
@@ -0,0 +1,41 @@
+# Idowu Timothy
+# idowutimothy12@yahoo.com
+# 08103236305
+
+import calendar
+cal = calendar.month(2018, 1)
+print "this is youngpans calendar:"
+print cal
+cal = calendar.month(2018, 2)
+print "this is youngpans calendar:"
+print cal
+cal = calendar.month(2018, 3)
+print "this is youngpans calendar:"
+print cal
+cal = calendar.month(2018, 4)
+print "this is youngpans calendar:"
+print cal
+cal = calendar.month(2018, 5)
+print "this is youngpans calendar:"
+print cal
+cal = calendar.month(2018, 6)
+print "this is youngpans calendar:"
+print cal
+cal = calendar.month(2018, 7)
+print "this is youngpans calendar:"
+print cal
+cal = calendar.month(2018, 8)
+print "this is youngpans calendar:"
+print cal
+cal = calendar.month(2018, 9)
+print "this is youngpans calendar:"
+print cal
+cal = calendar.month(2018, 10)
+print "this is youngpans calendar:"
+print cal
+cal = calendar.month(2018, 11)
+print "this is youngpans calendar:"
+print cal
+cal = calendar.month(2018, 12)
+print "this is youngpans calendar:"
+print cal
